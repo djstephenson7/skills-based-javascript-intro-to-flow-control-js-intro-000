80d321aab327971a7e4ceaf55c5b51927deb790d
Daniel James Stephenson <dan_0502@hotmail.co.uk>
2018-07-21 18:43:53 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,7 @@ function teenager(age) {
 function ageChecker(age) {
   if (age >= 13 && age <= 19)
   return ('You are a teenager!')
-  else{
+  else if (true) {
     (age <= 12)
     return ('You are a kid')
   }
